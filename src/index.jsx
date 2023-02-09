70464ec160dde62b4a2e88ecf445c67d3745fb7c
quratus <123387359+quratus@users.noreply.github.com>
2023-02-09 10:14:55 +0100

    Add files via upload

--- src/index.jsx
+++ src/index.jsx
@@ -8,28 +8,47 @@ const [isLoading, setIsLoading] = useState(false);
 
 async function generateText() {
   setIsLoading(true);
-  const issueResponse = await api.asApp().requestJira(`https://quratus.atlassian.net/rest/api/3/issue/${issueKey}?fields=summary,description`);
-  const { summary, description } = (await issueResponse.json()).fields;
+  const issueResponse = await api.asApp().requestJira(route`/rest/api/3/issue/${issueKey}?fields=summary,description`);
+
+  const responseJson = await issueResponse.json();
+
+  if (!responseJson || !responseJson.fields || !responseJson.fields.summary || !responseJson.fields.description) {
+    console.error(`Error: The response from the Jira API does not contain the expected fields. Response JSON: ${JSON.stringify(responseJson)}`);
+    responseJson = { fields: { summary: 'Create a Hello World Jira plugin', description: 'As developer, I want to create a hello world plugin for jira, using @forge/ui Library' }};
+   }
+
+const { summary, description } = responseJson.fields;
+
+console.log(`Sending fields: summary: "${summary}", description: "${description}"`);
 
   // send the summary and description to the backend
-  const backendResponse = await api.fetch(`/generate-text`, {
+  const backendResponse = await api.fetch(`http://localhost:3000/generate-text`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ summary, description })
+    body: JSON.stringify({ summary, description })  
   });
+  
+  if (backendResponse.ok) {
+    console.log("backendResponse ran successfully");
+  } else {
+    console.error(`backendResponse failed with status code: ${backendResponse.status}`);
+  }
+  
   const { generatedText } = await backendResponse.json();
+
 
   setGeneratedText(generatedText);
   setIsLoading(false);
 }
 
-return (
-<Fragment>
-<Button text="Generate Story" onClick={generateText} />
-{isLoading && <Text content="Loading..." />}
 
-{generatedText && <Text content={generatedText} />}
-</Fragment>
+return (
+  <Fragment>
+    <Button text="Generate Story" onClick={generateText} />
+    {isLoading && <Text content="Loading..." />}
+    {generatedText && <Text content={generatedText} />}
+    {generatedText && <Text content={`Sent fields: summary: "${summary}", description: "${description}"`} />}
+  </Fragment>
 );
 };
 
--- src/openai-backend.js
+++ src/openai-backend.js
@@ -3,35 +3,44 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const { route } = require('@forge/api');
 
+// const https = require('https');
+
+
+
+
+
 const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+  res.send('Welcome to the OpenAI backend server!');
+});
+
 app.use(bodyParser.json());
 
 const OPENAI_API_KEY = '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';
 
 app.post('/generate-text', async (req, res) => {
-const { summary, description } = req.body;
+  const { summary, description } = req.body;
 
-try {
+  try {
     const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
-        prompt: `I want you to act as a sharp and structured product Owner. Please write a comprehensive User Story based on " ${summary} ${description} " with acceptance criteria, notes, limitations and suggestions for subtasks.`,
-        max_tokens: 2048,
-        api_key: OPENAI_API_KEY,
+      prompt: `I want you to act as a sharp and structured product Owner. Please write a comprehensive User Story based on " ${summary} ${description} " with acceptance criteria, notes, limitations and suggestions for subtasks.`,
+      max_tokens: 2048,
+      api_key: OPENAI_API_KEY,
     });
 
     const generatedText = response.data.choices[0].text;
 
-    // Do any necessary post-processing of the output here
-    // ...
-
     res.send({ generatedText });
-} catch (error) {
+  } catch (error) {
     console.error(error);
     res.send({ error: 'Error generating text' });
-}
+  }
 });
 
-app.listen(3000, () => {
-console.log('Server listening on port 3000');
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
 
 module.exports = { route: route({app}) };
